d0e0bfdead23961cc47a022ab586c46d66e532a6
Chen Xiao <chenxiao770117@gmail.com>
2024-12-09 22:21:53 +0800

    update

--- lib/simple_svg.dart
+++ lib/simple_svg.dart
@@ -4,12 +4,12 @@ class SimpleSvg {
   Svg? svg;
 
   SimpleSvg(num width, num height) {
-    svg = Svg(width ,height);
+    svg = Svg(width, height);
   }
 
   String out() {
     final outBuffer = StringBuffer();
-    
+
     outBuffer.write('<svg\n');
     outBuffer.write('    version="1.1"\n');
     outBuffer.write('    xmlns="http://www.w3.org/2000/svg"\n');
@@ -17,7 +17,7 @@ class SimpleSvg {
     outBuffer.write('    width="${svg?.width}" height="${svg?.height}"\n');
     outBuffer.write('    >\n');
     outBuffer.write('</svg>\n');
-    
+
     return outBuffer.toString();
   }
 }
--- test/basic/empty_svg_test.dart
+++ test/basic/empty_svg_test.dart
@@ -6,9 +6,9 @@ import 'package:simple_svg/simple_svg.dart';
 void main() {
   test('basic empty svg out', () async {
     final simpleSvg = SimpleSvg(30, 20);
-    final file = new File('showcase/basic/empty.svg');
+    final file = File('showcase/basic/empty.svg');
     final emptySvgFile = await file.readAsString();
-    
+
     expect(simpleSvg.out(), emptySvgFile);
   });
 }
