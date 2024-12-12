2435bd70725828c0916be39f3fac9dc4d5252c06
Chen Xiao <chenxiao770117@gmail.com>
2024-12-12 16:04:33 +0800

    update

--- lib/defines/svg.dart
+++ lib/defines/svg.dart
@@ -8,18 +8,10 @@ class Svg {
 
   Svg(this.width, this.height);
   
-  String defShape(shape: Shape) {
+  String defShape(Shape shape) {
     widgetIdCount += 1;
-    shapeDefineMap.
+    String shapeId = 's${widgetIdCount}';
+    shapeDefineMap[shapeId] = shape;
+    return shapeId;
   }
 }
-
-  (let* ([new_widget_index (add1 (SVG-widget_id_count (*SVG*)))]
-         [shape_id (format "s~a" new_widget_index)])
-
-    (set-SVG-widget_id_count! (*SVG*) new_widget_index)
-
-    (hash-set! (SVG-shape_define_map (*SVG*)) shape_id shape)
-
-    shape_id))
-
--- test/defines/svg_test.dart
+++ test/defines/svg_test.dart
@@ -1,10 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_svg/defines/svg.dart';
+import 'package:simple_svg/defines/rect.dart';
 
 void main() {
   test('svg', () {
     final svg = Svg(30, 20.0);
     expect(svg.width, 30.0);
     expect(svg.height, 20.0);
+  });
+  
+  test('defShape', () {
+      final svg = Svg(30, 20.0);
+
+      final rect1 = Rect(30.0, 20.0);
+      svg.defShape(rect1);
+      expect(svg.shapeDefineMap.length, 1);
+
+      final rect2 = Rect(30.0, 20.0);
+      svg.defShape(rect2);
+      expect(svg.shapeDefineMap.length, 2);
   });
 }
