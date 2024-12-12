febf3f14d4ce5512507a74b4b3f2dbc055bcee19
Chen Xiao <chenxiao770117@gmail.com>
2024-12-12 11:37:10 +0800

    update

--- lib/defines/svg.dart
+++ lib/defines/svg.dart
@@ -7,4 +7,19 @@ class Svg {
   Map<String, Shape> shapeDefineMap = {};
 
   Svg(this.width, this.height);
+  
+  String defShape(shape: Shape) {
+    widgetIdCount += 1;
+    shapeDefineMap.
+  }
 }
+
+  (let* ([new_widget_index (add1 (SVG-widget_id_count (*SVG*)))]
+         [shape_id (format "s~a" new_widget_index)])
+
+    (set-SVG-widget_id_count! (*SVG*) new_widget_index)
+
+    (hash-set! (SVG-shape_define_map (*SVG*)) shape_id shape)
+
+    shape_id))
+
