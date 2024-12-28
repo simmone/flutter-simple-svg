19d2141460b18ef0080dad697e8172511e2045f1
Chen Xiao <chenxiao770117@gmail.com>
2024-12-28 17:25:39 +0800

    update

---
+++ lib/defines/widget.dart
@@ -0,0 +1,11 @@
+import 'sstyle.dart';
+
+class Widget {
+  final String shapeId;
+  (num, num)? at;
+  Sstyle? sstyle;
+  String? filterId;
+  String? markerStartId;
+  String? markerMidId;
+  String? markerEndId;
+}
