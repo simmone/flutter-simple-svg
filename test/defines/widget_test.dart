e5335905f7cfd49e7d833cf99b823951f9c56793
Chen Xiao <chenxiao770117@gmail.com>
2025-01-03 11:29:18 +0800

    update

--- test/defines/widget_test.dart
+++ test/defines/widget_test.dart
@@ -1,15 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_svg/defines/widget.dart';
+import 'package:simple_svg/defines/sstyle.dart';
 
 void main() {
   test('widget', () {
-    final widget = Widget("s1");
-    expect(widget.shapeId, "s1");
+    final widget = Widget('s1');
+    expect(widget.shapeId, 's1');
     expect(widget.sstyle, null);
   });
 
   test('format basic', () {
-    final widget = Widget("s1");
+    final widget = Widget('s1');
     expect(widget.format(), '<use xlink:href="#s1" />');
+  });
+  
+  test('format fill', () {
+      var sstyle = Sstyle();
+      sstyle.fill = '#BBC42A';
+
+      var widget = Widget('s1');
+      widget.sstyle = sstyle;
+
+      expect(widget.format(),
+        "<use xlink:href=\"#s1\" fill=\"#BBC42A\" />");
   });
 }
