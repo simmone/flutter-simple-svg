b317efe6a60c5fe38706945856fc84fce92e55a5
Chen Xiao <chenxiao770117@gmail.com>
2025-01-05 09:01:29 +0800

    update

--- lib/defines/widget.dart
+++ lib/defines/widget.dart
@@ -16,6 +16,30 @@ class Widget {
 
     outItems.add('<use xlink:href="#$shapeId"');
 
+    if (sstyle != null) {
+      outItems.add(sstyle!.format());
+    }
+
+    if ((at != null) && (at != (0, 0))) {
+      outItems.add('x="${at!.$1}" y="${at!.$2}"');
+    }
+
+    if (filterId != null) {
+      outItems.add('filter="url(#$filterId)"');
+    }
+
+    if (markerStartId != null) {
+      outItems.add('marker-start="url(#$markerStartId)"');
+    }
+
+    if (markerMidId != null) {
+      outItems.add('marker-mid="url(#$markerMidId)"');
+    }
+
+    if (markerEndId != null) {
+      outItems.add('marker-end="url(#$markerEndId)"');
+    }
+
     outItems.add('/>');
 
     return outItems.join(' ');
--- test/defines/widget_test.dart
+++ test/defines/widget_test.dart
@@ -21,6 +21,27 @@ void main() {
     var widget = Widget('s1');
     widget.sstyle = sstyle;
 
-    expect(widget.format(), "<use xlink:href=\"#s1\" fill=\"#BBC42A\" />");
+    expect(widget.format(), '<use xlink:href="#s1" fill="#BBC42A" />');
+  });
+
+  test('format at', () {
+    var widget1 = Widget('s1');
+    widget1.at = (0, 0);
+    expect(widget1.format(), '<use xlink:href="#s1" />');
+
+    var widget2 = Widget('s1');
+    widget2.at = (100, 50);
+    expect(widget2.format(), '<use xlink:href="#s1" x="100" y="50" />');
+  });
+
+  test('format else', () {
+    var widget = Widget('s1');
+    widget.filterId = 's2';
+    widget.markerStartId = 's3';
+    widget.markerMidId = 's4';
+    widget.markerEndId = 's5';
+
+    expect(widget.format(),
+        '<use xlink:href="#s1" filter="url(#s2)" marker-start="url(#s3)" marker-mid="url(#s4)" marker-end="url(#s5)" />');
   });
 }
