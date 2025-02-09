1c440ddc0077c1307f659092923f61e0695ac7e6
Chen Xiao <chenxiao770117@gmail.com>
2025-02-09 15:57:22 +0800

    update

--- test/group_test.dart
+++ test/group_test.dart
@@ -65,4 +65,87 @@ void main() {
 
     expect(svg.out(), rectSvgFile);
   });
+
+  test('group pattern', () async {
+    final svg = Svg(100, 100);
+
+    final rectId = svg.defShape(Rect(50, 50));
+    final line1Id = svg.defShape(Line((10, 0), (0, 50)));
+    final line2Id = svg.defShape(Line((0, 0), (10, 50)));
+
+    var lineSstyle = Sstyle();
+    lineSstyle.stroke = 'black';
+    lineSstyle.strokeWidth = 1;
+
+    var crossLineGroup = Group();
+
+    var widgetLine1 = Widget(line1Id);
+    widgetLine1.sstyle = lineSstyle;
+    crossLineGroup.placeWidget(widgetLine1);
+
+    var widgetLine2 = Widget(line2Id);
+    widgetLine2.sstyle = lineSstyle;
+    crossLineGroup.placeWidget(widgetLine2);
+
+    final crossLineGroupId = svg.addGroup(crossLineGroup);
+
+    var patternGroup = Group();
+
+    var rectSstyle = Sstyle();
+    rectSstyle.fill = 'orange';
+    rectSstyle.stroke = 'red';
+    rectSstyle.strokeWidth = 2;
+
+    var widgetRect = Widget(rectId);
+    widgetRect.sstyle = rectSstyle;
+    
+    patternGroup.placeWidget(widgetRect);
+    
+    var crossLineWidget1 = Widget(crossLineGroupId);
+    crossLineWidget1.at = (0, 0);
+    patternGroup.placeWidget(crossLineWidget1);
+
+    var crossLineWidget2 = Widget(crossLineGroupId);
+    crossLineWidget2.at = (10, 0);
+    patternGroup.placeWidget(crossLineWidget2);
+
+    var crossLineWidget3 = Widget(crossLineGroupId);
+    crossLineWidget3.at = (20, 0);
+    patternGroup.placeWidget(crossLineWidget3);
+
+    var crossLineWidget4 = Widget(crossLineGroupId);
+    crossLineWidget4.at = (30, 0);
+    patternGroup.placeWidget(crossLineWidget4);
+
+    var crossLineWidget5 = Widget(crossLineGroupId);
+    crossLineWidget5.at = (40, 0);
+    patternGroup.placeWidget(crossLineWidget5);
+    
+    final patternGroupId = svg.addGroup(patternGroup);
+    
+    var defaultGroup = Group();
+    
+    var patternWidget1 = Widget(patternGroupId);
+    patternWidget1.at = (0, 0);
+    defaultGroup.placeWidget(patternWidget1);
+
+    var patternWidget2 = Widget(patternGroupId);
+    patternWidget2.at = (50, 0);
+    defaultGroup.placeWidget(patternWidget2);
+
+    var patternWidget3 = Widget(patternGroupId);
+    patternWidget3.at = (0, 50);
+    defaultGroup.placeWidget(patternWidget3);
+
+    var patternWidget4 = Widget(patternGroupId);
+    patternWidget4.at = (50, 50);
+    defaultGroup.placeWidget(patternWidget4);
+
+    svg.addDefaultGroup(defaultGroup);
+
+    final file = File('showcase/group/group2.svg');
+    final rectSvgFile = await file.readAsString();
+
+    expect(svg.out(), rectSvgFile);
+  });
 }
