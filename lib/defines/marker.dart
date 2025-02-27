abe3929a0e40a3592120f2fba64b882024bc1859
Chen Xiao <chenxiao770117@gmail.com>
2025-02-27 17:48:41 +0800

    update

--- lib/defines/marker.dart
+++ lib/defines/marker.dart
@@ -17,18 +17,20 @@ class Marker implements Shape {
   num size;
   num x;
   String path;
+  
+  Marker(this.shape, this.size, this.x, this.path);
 
-  Marker(this.marker) {
-    switch(this.marker) {
-      case MarkerType.triangle1: return Marker(this.marker, 6, 1, '<path d="M0,0 L10,5 L0,10 z"');
-      case MarkerType.triangle2: return Marker(this.marker, 6, 1, '<path d="M0,0 L15,5 L0,10 z"');
-      case MarkerType.circle: return Marker(this.marker, 6, 5, '<circle r="5" cx="5" cy="5"');
-      case MarkerType.indent1: return Marker(this.marker, 6, 4, '<path d="M0,0 L10,5 L0,10 L5,5 z"');
-      case MarkerType.indent2: return Marker(this.marker, 6, 4, '<path d="M0,0 L15,5 L0,10 L5,5 z"');
-      case MarkerType.diamond1: return Marker(this.marker, 6, 1, '<path d="M3,0 L10,5 L3,10 L0,5 z"');
-      case MarkerType.diamond2: return Marker(this.marker, 6, 1, '<path d="M3,0 L15,5 L3,10 L0,5 z"');
-      case MarkerType.curve1: return Marker(this.marker, 6, 2, '<path d="M0,0 L10,5 L0,10 C0,10 5,5 0,0 z"');
-      case MarkerType.curve2: return Marker(this.marker, 6, 2, '<path d="M0,0 L15,5 L0,10 C0,10 5,5 0,0 z"');
+  Marker.fromShape(this.shape) {
+    switch(this.shape) {
+      case MarkerType.triangle1: this(this.shape, 6, 1, '<path d="M0,0 L10,5 L0,10 z"');
+      case MarkerType.triangle2: this(this.shape, 6, 1, '<path d="M0,0 L15,5 L0,10 z"');
+      case MarkerType.circle: this(this.shape, 6, 5, '<circle r="5" cx="5" cy="5"');
+      case MarkerType.indent1: this(this.shape, 6, 4, '<path d="M0,0 L10,5 L0,10 L5,5 z"');
+      case MarkerType.indent2: this(this.shape, 6, 4, '<path d="M0,0 L15,5 L0,10 L5,5 z"');
+      case MarkerType.diamond1: this(this.shape, 6, 1, '<path d="M3,0 L10,5 L3,10 L0,5 z"');
+      case MarkerType.diamond2: this(this.shape, 6, 1, '<path d="M3,0 L15,5 L3,10 L0,5 z"');
+      case MarkerType.curve1: this(this.shape, 6, 2, '<path d="M0,0 L10,5 L0,10 C0,10 5,5 0,0 z"');
+      case MarkerType.curve2: this(this.shape, 6, 2, '<path d="M0,0 L15,5 L0,10 C0,10 5,5 0,0 z"');
     }
   }
 
