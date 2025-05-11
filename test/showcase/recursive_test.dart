c8c2a7727d13dabdb12f6e6a6e67dcbd642ee670
Chen Xiao <chenxiao770117@gmail.com>
2025-05-11 09:42:09 +0800

    update

--- test/showcase/recursive_test.dart
+++ test/showcase/recursive_test.dart
@@ -2,41 +2,24 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_svg/define/shape/recursive.dart';
 
 void main() {
-  test('recursive', () {
-    final recursive = Recursive(30.0, 20.0);
-    expect(recursive.width, 30.0);
-    expect(recursive.height, 20.0);
-    expect(recursive.radiusX, null);
-    expect(recursive.radiusY, null);
-  });
+  test('recursive circles', () {
+    final svg = Svg(100, 100);
 
-  test('recursive_optional1', () {
-    final recursive = Recursive(30.0, 20.0, 10.0);
-    expect(recursive.width, 30.0);
-    expect(recursive.height, 20.0);
-    expect(recursive.radiusX, 10.0);
-    expect(recursive.radiusY, null);
-  });
+    final rectId = svg.defShape(Rect(100, 100));
 
-  test('recursive_optional2', () {
-    final recursive = Recursive(30.0, 20.0, 10.0, 5.0);
-    expect(recursive.width, 30.0);
-    expect(recursive.height, 20.0);
-    expect(recursive.radiusX, 10.0);
-    expect(recursive.radiusY, 5.0);
-  });
+    var defaultGroup = Group();
 
-  test('format1', () {
-    final recursive = Recursive(30, 20);
+    var sstyle = Sstyle();
+    sstyle.fill = '#BBC42A';
+    var widget = Widget(rectId);
+    widget.sstyle = sstyle;
 
-    expect(
-        recursive.format('1'), "    <recursive id=\"1\" width=\"30\" height=\"20\" />\n");
-  });
+    defaultGroup.placeWidget(widget);
+    svg.addDefaultGroup(defaultGroup);
 
-  test('format2', () {
-    final recursive = Recursive(30, 20, 10, 5.0);
+    final file = File('showcase/examples/recursive.svg');
+    final rectSvgFile = await file.readAsString();
 
-    expect(recursive.format('1'),
-        "    <recursive id=\"1\" width=\"30\" height=\"20\" rx=\"10\" ry=\"5.0\" />\n");
+    expect(svg.out(), rectSvgFile);
   });
 }
