cf282e39dad8c2ba2fb38d8969fbd4056ac8fa98
Chen Xiao <chenxiao770117@gmail.com>
2025-05-24 18:21:46 +0800

    update

--- test/showcase/fern_test.dart
+++ test/showcase/fern_test.dart
@@ -5,21 +5,28 @@ import 'package:complex/complex.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_svg/simple_svg.dart';
 
-Complex makePolar(num magnitude, num angle) {
-  final complexNumber = Complex(0, 1);
+(num, num) getEndPoint((num, num) startPoint, num length) {
+  final end = Complex.polar(length, pi * 2 * deg / 360);
 
-  return Complex(cos(angle) * magnitude) + (Complex(sin(angle) * magnitude) * complexNumber);
+  return (
+    (startPorint.$1 + end.imaginary) * (
+  
+    (
+        ((start_point.0 + end.re) * (10.0f64.powf(precision))).round() / (10.0f64.powf(precision)),
+        ((start_point.1 + end.im) * (10.0f64.powf(precision))).round() / (10.0f64.powf(precision)),
+    )
 }
+
 
 void main() {
   test('make poloar test', () async {
       expect(
-        makePolar(10, pi * 0.5),
+        Complex.polar(10, pi * 0.5),
         Complex(6.123233995736766e-16, 10.0)
       );
 
       expect(
-        makePolar(10, pi * 0.25),
+        Complex.polar(10, pi * 0.25),
         Complex(7.0710678118654755, 7.071067811865475)
       );
   });
